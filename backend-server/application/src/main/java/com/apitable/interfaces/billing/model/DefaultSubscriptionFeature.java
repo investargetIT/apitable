b1ff3b1cad4800947401d3b6a686944d98b5654d
Oliver <olimovier@gmail.com>
2024-04-24 14:39:10 +0800

    Update days

--- backend-server/application/src/main/java/com/apitable/interfaces/billing/model/DefaultSubscriptionFeature.java
+++ backend-server/application/src/main/java/com/apitable/interfaces/billing/model/DefaultSubscriptionFeature.java
@@ -218,7 +218,7 @@ public class DefaultSubscriptionFeature implements SubscriptionFeature {
 
     @Override
     public RemainTrashDays getRemainTrashDays() {
-        return new RemainTrashDays(365L);
+        return new RemainTrashDays(425L);
     }
 
     @Override
@@ -228,7 +228,7 @@ public class DefaultSubscriptionFeature implements SubscriptionFeature {
 
     @Override
     public RemainRecordActivityDays getRemainRecordActivityDays() {
-        return new RemainRecordActivityDays(365L);
+        return new RemainRecordActivityDays(425L);
     }
 
     @Override
